30e07017355b03bd0e16a4a7a30dcc1eb91e3cca
Pushkar <pushkarjeemain@gmail.com>
2024-11-14 03:51:55 +0530

    Update SP1.cpp

--- Easy code/SP1.cpp
+++ Easy code/SP1.cpp
@@ -3,18 +3,20 @@
 using namespace std;
 int main(){
   int arr[5]={2,4,1,8,3};
-  int min= INT_MAX;
-  int max= INT_MIN;
+  int smallest= INT_MAX;
+  int largest= INT_MIN;
   int size=5;
   for(int i=0;i<size;i++){
-      if(arr[i]<min){
-        min=arr[i];
-    }
-    if(arr[i]>max){
-        max=arr[i];
+  //     if(arr[i]<smallest){
+  //       smallest=arr[i];
+  //   }
+  //   if(arr[i]>largest){
+  //       largest=arr[i];
+  // }
+      smallest=min(arr[i],smallest);
+      largest=max(arr[i],largest);
   }
-  }
-  cout<<"smallest number\n"<<min<<endl;
-  cout<<"largest number\n"<<max<<endl;
+  cout<<"smallest number\n"<<smallest<<endl;
+  cout<<"largest number\n"<<largest<<endl;
   return 0;
 }
