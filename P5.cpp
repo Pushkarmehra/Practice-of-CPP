b8387a2394d92d246cfb380b1c75455194e396cc
Pushkar <pushkarjeemain@gmail.com>
2024-11-18 01:21:24 +0530

    Create P5.cpp

---
+++ P5.cpp
@@ -0,0 +1,30 @@
+// with nested loop
+// class Solution {
+// public:
+//     int singleNumber(vector<int>& nums) {
+//         vector<int>& nums
+//         int size=nums.size();
+//         for(int i=0;i<size;i++){
+//             for(int j=0;j<size;j++){
+//                 if(nums[i]==nums[j] && i!=j){
+//                     return nums[i];
+//                 }
+//                 else{
+//                     continue;
+//                 }
+//             }
+//         }
+//     }
+// };
+
+// linear solve 
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ans=0;
+       for(int val :nums){
+        ans=ans ^ val;
+       } 
+       return ans; }
+};
+/*using the linear we have to use one loop only so i use binary XOR ans using the vectorloop it become so help full */
